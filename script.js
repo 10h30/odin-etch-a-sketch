3b9e36df7826f15a228bfbff0b0f883b661b5dbd
Thuan Bui <9248622+10h30@users.noreply.github.com>
2025-02-23 11:17:04 +0900

    Comlete basic task

--- script.js
+++ script.js
@@ -2,10 +2,43 @@ const container = document.querySelector(".container")
 const containerWidth = document.querySelector(".container").clientWidth; 
 console.log(containerWidth)
 
-for (let i=0; i < 16*16; i++) {
-    const squareBox = document.createElement("div")
-    container.appendChild(squareBox)
-    squareBox.setAttribute('class', 'grid')
-    squareBox.style.width= containerWidth / 16 + "px";;
-    squareBox.style.height= containerWidth / 16 + "px";;
+const button = document.querySelector("button")
+button.addEventListener('click', () => {
+
+    let gridSize = prompt("Please enter grid size: ")  
+    makeGrid(gridSize)
 }
+)
+
+function makeGrid(size) {
+    container.innerHTML = ""
+    for (let i=0; i < size * size; i++) {
+        const squareBox = document.createElement("div")
+        container.appendChild(squareBox)
+        squareBox.setAttribute('class', 'grid')
+        squareBox.style.width= containerWidth / size + "px";;
+        squareBox.style.height= containerWidth / size + "px";;
+    }
+    const grid = document.querySelectorAll('.grid')
+    for (i of grid) {
+    i.addEventListener('mouseover', 
+    //e => e.target.classList.add('changecolor')
+    e => e.target.style.backgroundColor = getRandomColor()
+    )
+}  
+
+}
+makeGrid(16)
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+
+
+
